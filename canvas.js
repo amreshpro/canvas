6600f7013be4d0859151c19d45b64f2b46914623
Amresh Maurya <amresh.terminal@gmail.com>
2023-05-07 13:54:31 +0530

    basics of the canavs part2

--- canvas.js
+++ canvas.js
@@ -8,10 +8,11 @@ const context = canvas.getContext('2d');
 // context.fillRect(x,y,width,height)
 context.fillStyle = "#af3456"
 context.fillRect(100,100,100,100)
+context.fillStyle = "#af3123"
 context.fillRect(400,100,100,100)
-
+context.fillStyle = "#af7456"
 context.fillRect(100,300,100,100)
-
+context.fillStyle = "#ae3122"
 context.fillRect(100,500,100,100)
 
 
@@ -22,3 +23,14 @@ context.lineTo(300,100)
 context.lineTo(400,300)
 context.strokeStyle="#fa34a3"
 context.stroke();
+
+
+for(let i = 0;i<2000;++i){
+    // Arch /circle
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() *window.innerHeight;
+context.beginPath();
+context.arc(x,y,30,0,Math.PI*2,false);
+context.strokeStyle = "orange"
+context.stroke();
+}
